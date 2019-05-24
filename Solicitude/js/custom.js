74b4f9f97c747fa804952d25f2d4e9abf3a24da6
Everton4732 <43539134+Everton4732@users.noreply.github.com>
2019-05-24 16:47:17 -0300

    Add files via upload
    Site criado junto com o @alexishikawa

---
+++ Solicitude/js/custom.js
@@ -0,0 +1,50 @@
+
+$(document).ready(function() { 
+        $(window).scroll(function() { 
+          if($(this).scrollTop() > 300) { 
+              $('.navbar').addClass('solid'); 
+          } else {
+              $('.navbar').removeClass('solid'); 
+          }
+        });
+});
+
+
+
+$(document).ready(function () { 
+    $(document).click(function (event) {
+        var clickover = $(event.target); 
+        var _opened = $(".navbar-collapse").hasClass("show"); 
+        if (_opened === true && !clickover.hasClass("navbar-toggler")) { 
+            $(".navbar-toggler").click(); 
+        }
+    });
+});
+
+
+
+$(document).ready(function(){ 
+  $("a").on('click', function(event) {
+    if (this.hash !== "") { 
+      event.preventDefault();
+
+      
+      var hash = this.hash;
+      $('html, body').animate({ 
+        scrollTop: $(hash).offset().top 
+      }, 800, function(){
+   
+  
+        window.location.hash = hash;
+      });
+    } 
+  });
+});
+
+
+$(document).ready(function(){
+$(window).scroll(function(){ 
+    $(".arrow").css("opacity", 1 - $(window).scrollTop() / 250); 
+  });
+});
+
